e6153832f7a80ef404e348120783fb73618d14e0
Gabriel Estevam Maciel <gabrielestevammaciel1@gmail.com>
2020-12-13 16:36:33 -0300

    DepartmentListController

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,10 +1,12 @@
 package gui;
 
 import java.net.URL;
-import java.time.chrono.MinguoChronology;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -13,9 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable
 {
+	private DepartmentService service;
+	
 	@FXML
 	private TableView<Department> tableViewDepartment;
 	
@@ -28,11 +33,19 @@ public class DepartmentListController implements Initializable
 	@FXML
 	private Button btNew;
 	
+	private ObservableList<Department> obsList;
+	
 	@FXML
 	public void onBtNewAction()
 	{
 		System.out.println("OnBtNewAction");
 	}
+	
+	public void setDepartmentService(DepartmentService service)
+	{
+		this.service = service;
+	}
+	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
@@ -47,5 +60,15 @@ public class DepartmentListController implements Initializable
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
-
+	
+	public void updateTableView()
+	{
+		if(service == null)
+		{
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+	}
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable
 {
@@ -37,7 +38,7 @@ public class MainViewController implements Initializable
 	@FXML
 	public void onMenuItemDepartmentAction()
 	{
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
@@ -55,34 +56,6 @@ public class MainViewController implements Initializable
 	
 	private synchronized void loadView(String absoluteName)
 	{
-		/*Função para carregar a tela de about
-		 	* essa função tem como parametro o caminho para o fxml da tela about
-		 	* que vai ser informada pelo programador;
-		 	* 
-		 	*  Seu funcionamento esta incluido dentro de um bloco try-catch, pois
-		 	*  pode gerar uma exceção, essa exceção esta sendo tratada pela nossa classe
-		 	*  Constraints.java, que vai retornar um alert;
-		 	*  
-		 	*   Na função mainView foi necessario criar um atributo do tipo static que 
-		 	*   vai contem a tela principal (scene) e tambem foi cria uma função para
-		 	*  retornar essa tela, tambem do tipo static é do tipo Scene;
-		 	*  
-		 	*Apatir desse analogia não sera necessario carregar uma outra tela sobrepondo a "mainView",
-		 	* pois a logica fica contida em pegar o conteudo da tela de about e 
-		 	* inserir na tela principal, ficando apenas uma tela em produção
-		 	*
-		 	*  FXMLLoader "loader" - objeto responsavel por carregar os recursos da dela
-		 	*  sendo o caminho aonde esta salvo o "fxml" ate sua criação em "fmxl"
-		 	*  
-		 	*  VBox newVBox - objeto responsavel por mostra (carregar) a tela no tipo "VBox"
-		 	*
-		 	*	Scene "MainScene" - objeto responsavel por pegar os recursos da "mainView"
-		 	*	
-		 	*	VBox "mainVBox" - objeto responsavel por pegar os conteudos da "mainview"
-		 	*
-		 	*	Node "mainMenu" - objeto responsavel por pegar os filhos (tags) 
-		 	*	
-		 */
 		try
 		{
 			//carrega o fxml
@@ -98,8 +71,39 @@ public class MainViewController implements Initializable
 			Node mainMenu = mainVBox.getChildren().get(0); //"pega" os filhos da tela principal (as tags <>)
 			
 			mainVBox.getChildren().clear(); //limpa a tela principal
-		mainVBox.getChildren().add(mainMenu); //adiciona a nova tela ja com o conteudo do "mainAbout"
+			mainVBox.getChildren().add(mainMenu); //adiciona a nova tela ja com o conteudo do "mainAbout"
 			mainVBox.getChildren().addAll(newVBox.getChildren()); //passa o novos filhos(tags) da tela about
+		}
+		catch(IOException e)
+		{
+			Alerts.showAlert("IO Exception", "Error on loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private synchronized void loadView2(String absoluteName)
+	{
+		//função temporaria por enquanto
+		try
+		{
+			//carrega o fxml
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			
+			VBox newVBox = loader.load(); //objeto do tipo VBox para carregar a cena
+			
+			Scene mainScene = Main.getMainScene(); //"pega" a tela principal
+			
+			//esse objeto é responsavel por pegar todos os recursos da tela princiapl
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0); //"pega" os filhos da tela principal (as tags <>)
+			
+			mainVBox.getChildren().clear(); //limpa a tela principal
+			mainVBox.getChildren().add(mainMenu); //adiciona a nova tela ja com o conteudo do "mainAbout"
+			mainVBox.getChildren().addAll(newVBox.getChildren()); //passa o novos filhos(tags) da tela about
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
 		}
 		catch(IOException e)
 		{
